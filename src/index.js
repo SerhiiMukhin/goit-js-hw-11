33012d53241e34d0763372114284007b8919e307
SerhiiMukhin <sergio.lerone.ua@gmail.com>
2023-01-09 14:26:18 +0200

    template problem

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import debounce from 'lodash.debounce';
-// import { fetchImages } from './fetchImages';
-// import axios from 'axios';
 
 import PixabayApiService from './pixabay-api-service';
+import pictureTemplate from './templates/template.hbs';
 
 const refs = {
   form: document.querySelector('#search-form'),
-  container: document.querySelector(".gallery"),
-  loadMoreBtn: document.querySelector(".load-more"),
-}
+  container: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
 
 refs.form.addEventListener('submit', onSubmit);
 refs.loadMoreBtn.addEventListener('click', loadMore);
@@ -17,12 +16,12 @@ const pixabayApiService = new PixabayApiService();
 
 function onSubmit(event) {
   event.preventDefault();
-  
+
   pixabayApiService.query = event.currentTarget.elements.searchQuery.value;
   pixabayApiService.resetPage();
   pixabayApiService.fetchImages().then(data => console.log(data.hits));
 }
 
 function loadMore(event) {
-
   pixabayApiService.fetchImages().then(data => console.log(data.hits));
+}
